fa4e90bf34bda8d24c86f2a2c9d8af56ce4fa8a3
Emma3TC-main <U22329385@utp.edu.pe>
2025-06-04 17:38:59 -0500

    Primer commit: estructura base del proyecto con módulos Maven

---
+++ sistema-admin/src/main/java/com/grupo1/sistema/admin/SistemaAdmin.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.grupo1.sistema.admin;
+
+/**
+ *
+ * @author PC
+ */
+public class SistemaAdmin {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ sistema-app/src/main/java/com/grupo1/sistema/app/SistemaApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.grupo1.sistema.app;
+
+/**
+ *
+ * @author PC
+ */
+public class SistemaApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
