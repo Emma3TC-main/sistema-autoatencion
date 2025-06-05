287fe11e8e5af849cce064ebd74db3420b38b755
Emma3TC-main <U22329385@utp.edu.pe>
2025-06-05 15:41:36 -0500

    creacion de modulo dañado y reparacion del pom.xml

---
+++ sistema-mesas/src/main/java/com/grupo1/sistema/mesas/SistemaMesas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.grupo1.sistema.mesas;
+
+/**
+ *
+ * @author emma3tc
+ */
+public class SistemaMesas {
+
+    public static void main(String[] args) {
+        System.out.println("Test!");
+    }
+}
